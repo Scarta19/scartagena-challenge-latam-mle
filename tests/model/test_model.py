61a632771f46328c5b337e3260be5e2b6bc7759d
Scarta19 <santwhm@gmail.com>
2025-05-10 12:58:57 -0500

    test(model): restore original challenge test_model.py

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -10,16 +10,16 @@ from challenge.model import DelayModel
 
 class TestModel(unittest.TestCase):
     FEATURES_COLS = [
-        "OPERA_Aerolineas Argentinas",
+        "OPERA_Latin American Wings", 
         "MES_7",
-        "MES_11",
-        "OPERA_LATAM",
-        "OPERA_SKY Airline",
+        "MES_10",
+        "OPERA_Grupo LATAM",
         "MES_12",
         "TIPOVUELO_I",
-        "OPERA_JetSMART SPA",
-        "MES_10",
-        "MES_1",
+        "MES_4",
+        "MES_11",
+        "OPERA_Sky Airline",
+        "OPERA_Copa Air",
     ]
 
     TARGET_COL = ["delay"]
@@ -32,37 +32,51 @@ class TestModel(unittest.TestCase):
 
     def test_model_preprocess_for_training(self):
         features, target = self.model.preprocess(data=self.data, target_column="delay")
+
         assert isinstance(features, pd.DataFrame)
         assert features.shape[1] == len(self.FEATURES_COLS)
         assert set(features.columns) == set(self.FEATURES_COLS)
+
         assert isinstance(target, pd.DataFrame)
         assert target.shape[1] == len(self.TARGET_COL)
         assert set(target.columns) == set(self.TARGET_COL)
 
     def test_model_preprocess_for_serving(self):
         features = self.model.preprocess(data=self.data)
+
         assert isinstance(features, pd.DataFrame)
         assert features.shape[1] == len(self.FEATURES_COLS)
         assert set(features.columns) == set(self.FEATURES_COLS)
 
     def test_model_fit(self):
-        features, target = self.model.preprocess(data=self.data, target_column="delay")
+        features, target = self.model.preprocess(
+            data=self.data, target_column="delay"
+        )
+
         _, features_validation, _, target_validation = train_test_split(
             features, target, test_size=0.33, random_state=42
         )
+
         self.model.fit(features=features, target=target)
+
         predicted_target = self.model._model.predict(features_validation)
+
         report = classification_report(
-            target_validation, predicted_target, output_dict=True, zero_division=0
+            target_validation, predicted_target, output_dict=True
         )
-        assert "0" in report and "1" in report
-        assert 0.0 <= report["0"]["recall"] <= 1.0
-        assert 0.0 <= report["1"]["recall"] <= 1.0
+
+        assert report["0"]["recall"] < 0.60
+        assert report["0"]["f1-score"] < 0.70
+        assert report["1"]["recall"] > 0.60
+        assert report["1"]["f1-score"] > 0.30
 
     def test_model_predict(self):
-        features, target = self.model.preprocess(data=self.data, target_column="delay")
-        self.model.fit(features=features, target=target)
+        features = self.model.preprocess(data=self.data)
+
         predicted_targets = self.model.predict(features=features)
+
         assert isinstance(predicted_targets, list)
         assert len(predicted_targets) == features.shape[0]
-        assert all(isinstance(pred, int) for pred in predicted_targets)
+        assert all(
+            isinstance(predicted_target, int) for predicted_target in predicted_targets
+        )
