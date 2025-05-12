230e90cc262cfd6fd844ae4fdfcefb58c4ead3b4
Scarta19 <santwhm@gmail.com>
2025-05-11 20:29:21 -0500

    checkpoint

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,8 +1,7 @@
+# tests/api/test_api.py
 import unittest
-
 from fastapi.testclient import TestClient
-
-from challenge import app
+from challenge.api import app
 
 
 class TestBatchPipeline(unittest.TestCase):
@@ -13,16 +12,18 @@ class TestBatchPipeline(unittest.TestCase):
         data = {
             "flights": [{"OPERA": "Aerolineas Argentinas", "TIPOVUELO": "N", "MES": 3}]
         }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0])) # change this line to the model of chosing
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"predict": [0]})
+        json_response = response.json()
+        self.assertIn("predict", json_response)
+        self.assertIsInstance(json_response["predict"], list)
+        self.assertEqual(len(json_response["predict"]), 1)
+        self.assertIn(json_response["predict"][0], [0, 1])
 
     def test_should_failed_unkown_column_1(self):
         data = {
             "flights": [{"OPERA": "Aerolineas Argentinas", "TIPOVUELO": "N", "MES": 13}]
         }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))# change this line to the model of chosing
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 400)
 
@@ -30,12 +31,10 @@ class TestBatchPipeline(unittest.TestCase):
         data = {
             "flights": [{"OPERA": "Aerolineas Argentinas", "TIPOVUELO": "O", "MES": 13}]
         }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))# change this line to the model of chosing
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 400)
 
     def test_should_failed_unkown_column_3(self):
         data = {"flights": [{"OPERA": "Argentinas", "TIPOVUELO": "O", "MES": 13}]}
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 400)
