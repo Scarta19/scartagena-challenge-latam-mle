ff6a0780264f8adfe6180320e4fca74092c0cc18
Scarta19 <santwhm@gmail.com>
2025-05-11 20:46:09 -0500

    checkpoint

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,40 +1,84 @@
-# tests/api/test_api.py
-import unittest
-from fastapi.testclient import TestClient
-from challenge.api import app
+from typing import List
+
+import joblib
+import pandas as pd
+from fastapi import FastAPI
+from fastapi import HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
 
 
-class TestBatchPipeline(unittest.TestCase):
-    def setUp(self):
-        self.client = TestClient(app)
+class FlightItem(BaseModel):
+    OPERA: str
+    TIPOVUELO: str
+    MES: int
 
-    def test_should_get_predict(self):
-        data = {
-            "flights": [{"OPERA": "Aerolineas Argentinas", "TIPOVUELO": "N", "MES": 3}]
+
+class FlightRequest(BaseModel):
+    flights: List[FlightItem]
+
+
+@app.get("/health", status_code=200)
+def get_health() -> dict:
+    return {"status": "OK"}
+
+
+@app.post("/predict")
+def predict(flight_request: FlightRequest):
+    try:
+        df = pd.DataFrame([f.dict() for f in flight_request.flights])
+
+        required_cols = {"OPERA", "TIPOVUELO", "MES"}
+        if not required_cols.issubset(df.columns):
+            raise ValueError("Missing required columns")
+
+        valid_operas = {
+            "American Airlines",
+            "Air Canada",
+            "Air France",
+            "Aeromexico",
+            "Aerolineas Argentinas",
+            "Austral",
+            "Avianca",
+            "Alitalia",
+            "British Airways",
+            "Copa Air",
+            "Delta Air",
+            "Gol Trans",
+            "Iberia",
+            "K.L.M.",
+            "Qantas Airways",
+            "United Airlines",
+            "Grupo LATAM",
+            "Sky Airline",
+            "Latin American Wings",
+            "Plus Ultra Lineas Aereas",
+            "JetSmart SPA",
+            "Oceanair Linhas Aereas",
+            "Lacsa",
         }
-        response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 200)
-        json_response = response.json()
-        self.assertIn("predict", json_response)
-        self.assertIsInstance(json_response["predict"], list)
-        self.assertEqual(len(json_response["predict"]), 1)
-        self.assertIn(json_response["predict"][0], [0, 1])
 
-    def test_should_failed_unkown_column_1(self):
-        data = {
-            "flights": [{"OPERA": "Aerolineas Argentinas", "TIPOVUELO": "N", "MES": 13}]
-        }
-        response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 400)
+        if not df["OPERA"].isin(valid_operas).all():
+            raise ValueError("Invalid OPERA values")
 
-    def test_should_failed_unkown_column_2(self):
-        data = {
-            "flights": [{"OPERA": "Aerolineas Argentinas", "TIPOVUELO": "O", "MES": 13}]
-        }
-        response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 400)
+        if not df["TIPOVUELO"].isin(["I", "N"]).all():
+            raise ValueError("Invalid TIPOVUELO values")
 
-    def test_should_failed_unkown_column_3(self):
-        data = {"flights": [{"OPERA": "Argentinas", "TIPOVUELO": "O", "MES": 13}]}
-        response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 400)
+        if not df["MES"].between(1, 12).all():
+            raise ValueError("Invalid MES values")
+
+        X = model.preprocess(df)
+        preds = model.predict(X)
+        return {"predict": preds}
+
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid input data")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("challenge.api:app", host="0.0.0.0", port=8080)
